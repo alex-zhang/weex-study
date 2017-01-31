7943e0fe232bb20bb2486880ac94debd8606d5d8
alex zhang <gzane24@gmail.com>
2017-02-01 01:01:10 +0800

    modify page

--- src/home.vue
+++ src/home.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <text>hello</text>
+    <text>{{info}}</text>
   </div>
 </template>
 <style>
 </style>
 <script>
-
+  debugger;
+//  export default {
+//    data() {
+//      return {
+//        info: JSON.parse(weex.config)
+//      }
+//    },
+//
+//    created() {
+//      console.log(JSON.parse(weex.config));
+//    }
+//  }
 </script>
